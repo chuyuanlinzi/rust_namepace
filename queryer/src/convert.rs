99afe03b2bd104a9eafcc7427b6c678e2fa7609a
weilz1 <weilz1@ziroom.com>
2024-02-18 18:26:00 +0800

    添加queryer项目

--- queryer/src/convert.rs
+++ queryer/src/convert.rs
@@ -12,8 +12,21 @@ pub struct Sql<'a> {
     pub(crate) source: &'a str,
     pub(crate) order_by: Vec<(String, bool)>,
     pub(crate) offset: Option<i64>,
-    pub(crate) limit: Option<i64>,
+    pub(crate) limit: Option<usize>,
 }
+
+// 因为 Rust trait 的孤儿规则，我们如果想要对已有的类型实现已有的 trait，
+// 需要简单包装一下
+
+pub struct Expression(pub(crate) Box<SqlExpr>);
+pub struct Operation(pub(crate) SqlBinaryOperator);
+pub struct Projection<'a>(pub(crate) &'a SelectItem);
+pub struct Source<'a>(pub(crate) &'a [TableWithJoins]);
+pub struct Order<'a>(pub(crate) &'a OrderByExpr);
+pub struct Offset<'a>(pub(crate) &'a SqlOffset);
+pub struct Limit<'a>(pub(crate) &'a SqlExpr);
+pub struct Value(pub(crate) SqlValue);
+
 
 impl<'a> TryFrom<&'a Statement> for Sql<'a> {
     type Error = anyhow::Error;
@@ -31,6 +44,8 @@ impl<'a> TryFrom<&'a Statement> for Sql<'a> {
                     // SetExpr::Select(statement) => statement.as_ref(),
                     _ => return Err(anyhow!("We only support Select Query at the moment")),
                 };
+
+                let source = Source(table_with_joins).try_into()?;
             }
             _ => Err(anyhow!("We Only Support Select Query at the moment"))
         }
--- queryer/src/lib.rs
+++ queryer/src/lib.rs
@@ -1,5 +1,72 @@
+extern crate core;
+
+use std::ops::{Deref, DerefMut};
+use polars::frame::DataFrame;
+use polars::prelude::CsvWriter;
+use anyhow::Result;
+use sqlparser::parser::Parser;
+use tracing::info;
+use crate::convert::Sql;
+use crate::dialect::TyrDialect;
+
 mod dialect;
 mod convert;
+
+#[derive(Debug)]
+pub struct DataSet(DataFrame);
+
+/// 让 DataSet 用起来和 DataFrame 一致
+impl Deref for DataSet{
+    type Target = DataFrame;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for DataSet {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl DataSet {
+    /// 从 DataSet 转换成 csv
+    pub fn to_csv(&self) -> Result<String> {
+        let mut buf = Vec::new();
+        let writer = CsvWriter::new(&mut buf);
+        writer.finish(self)?;
+        Ok(String::from_utf8(buf)?)
+    }
+}
+
+
+/// 从 from 中获取数据，从 where 中过滤，最后选取需要返回的列
+pub async fn query<T: AsRef<str>>(sql: T) -> Result<DataSet> {
+    let ast = Parser::parse_sql(&TyrDialect::default(), sql.as_ref())?;
+
+    if ast.len() != 1 {
+        return Err(anyhow!("Only support single sql at the moment"));
+    }
+
+    let sql = &ast[0];
+
+    let Sql{
+        source,
+        condition,
+        selection,
+        offset,
+        limit,
+        order_by,
+    } = sql.try_into()?;
+
+    info!("retrieving data from source: {]", source);
+
+    // 从 source 读入一个 DataSet
+    // let ds = detect_content
+
+}
+
 
 // pub fn add(left: usize, right: usize) -> usize {
 //     left + right
